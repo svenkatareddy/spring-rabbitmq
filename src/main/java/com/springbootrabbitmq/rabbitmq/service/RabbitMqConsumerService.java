fec6b04549b3f07a6db1f790de85fee1406b8850
Venkata Reddy <venkata.reddy@udaan.com>
2023-06-20 16:40:47 +0530

    First commit

--- src/main/java/com/springbootrabbitmq/rabbitmq/service/RabbitMqConsumerService.java
+++ src/main/java/com/springbootrabbitmq/rabbitmq/service/RabbitMqConsumerService.java
@@ -4,8 +4,6 @@ import com.springbootrabbitmq.rabbitmq.dto.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
